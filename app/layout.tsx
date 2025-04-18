77278da409c6ae9533c6d004b2d0605c2fac5be0
Charanya-A <141047196+Charanya-A@users.noreply.github.com>
2025-04-18 08:18:52 +0530

    Refactor RootLayout component for improved structure and styling consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,29 @@
-
-import './globals.css'
-import React, { PropsWithChildren } from 'react'
-import LeftImage from '@/components/LeftImage'
-import NavigationBar from "@/components/navigation-bar/NavigationBar";
+import './globals.css';
+import React, { PropsWithChildren } from 'react';
+import LeftImage from '@/components/LeftImage';
+import NavigationBar from '@/components/navigation-bar/NavigationBar';
 
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <html lang="en">
       <body className="flex min-h-screen bg-zinc-900 text-white">
-        {/* Left Section */}
-        <div className="fixed left-0 top-0 w-[25%] h-screen z-10">
+        {/* Sidebar with Image */}
+        <div className="fixed left-0 top-0 w-[22%] h-screen z-10">
           <LeftImage />
         </div>
 
-        {/* Main Section - Wider */}
-        <div className="ml-[26%] mr-[10%] w-[64%] flex flex-col items-center justify-center min-h-screen px-5">
+        {/* Main Content Area */}
+        <main className="ml-[22%] mr-[8%] w-[70%] flex flex-col items-start justify-center min-h-screen px-5">
           {children}
-        </div>
+        </main>
 
-        {/* Right Navbar */}
-        <div className="fixed right-6 top-1/2 -translate-y-1/2 z-20">
+        {/* Vertical Navigation Bar */}
+        <div className="fixed right-4 top-1/2 -translate-y-1/2 z-20">
           <NavigationBar />
         </div>
       </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
-
+export default RootLayout;
