d1ed14915ff7580d3405f6334b8b9fd4fa1b5406
Charanya-A <141047196+Charanya-A@users.noreply.github.com>
2025-04-18 08:19:12 +0530

    Refactor WorkExp component for improved heading spacing and alignment

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,10 +1,9 @@
 export default function WorkExp() {
     return (
       <section className="w-full px-6 pt-4 pb-12 bg-zinc-900 text-white" id="work-experience">
-        <h1 className="text-4xl font-bold text-gray-300 mb-8">Work Experience</h1>
+        <h1 className="text-4xl font-bold text-gray-300 mb-10 sm:pl-25 md:pl-27">Work Experience</h1>
   
         <div className="space-y-6 max-w-3xl mx-auto">
-          {/* Card 1 */}
           <div className="border border-gray-700 rounded-xl p-6 shadow-md bg-zinc-800 text-white space-y-3">
             <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
               <h2 className="text-xl font-semibold">FullStack AI Developer Intern</h2>
